4c58139e7c566f0752da41ca0e35d6f8aaccde67
asafonov <me@asafonov.org>
2023-06-07 15:46:50 +0200

    switching to nodejs because of the telethon bugs

--- tgirc/__main__.py
+++
@@ -1,7 +0,0 @@
-def main():
-    import tgirc.irc, tgirc.telegram
-    tgirc.irc.start(privmsg = tgirc.telegram.send, get_messages = tgirc.telegram.check_messages, disconnect=tgirc.telegram.disconnect)
-    tgirc.telegram.client.run_until_disconnected()
-
-if __name__ == "__main__":
-    main()
--- tgirc/api.py
+++
@@ -1,9 +0,0 @@
-import tgirc.utils
-import os
-
-config = tgirc.utils.get_config()
-config_dir = tgirc.utils.get_config_dir()
-session_file = config_dir + '/tgirc.session'
-
-if not os.path.exists(config_dir):
-    os.mkdir(config_dir)
