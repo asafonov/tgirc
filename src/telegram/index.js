ddd5f2ddc885019413ce9fbaa14291e604f510ad
asafonov <me@asafonov.org>
2023-06-09 10:59:52 +0200

    typo fix

--- src/telegram/index.js
+++ src/telegram/index.js
@@ -1,3 +1,10 @@
 const {TelegramClient} = require('telegram')
-const {StringSession} = require('telegram/session')
-const cache = reqiure('../config').init('cache')
+const {StringSession} = require('telegram/sessions')
+const cache = require('../config').init('cache')
+
+const init = (apiId, apiHash) => {
+}
+
+module.exports = {
+  init: init
+}
