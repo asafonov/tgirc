af6f6f2f8eabe39dc27e294f7ee247c228765f64
asafonov <me@asafonov.org>
2024-12-25 14:55:39 +0000

    minor improvements for allowing message modifications before sending between irc and telegram

--- src/tgirc/index.js
+++ src/tgirc/index.js
@@ -1,12 +1,20 @@
 const telegram = require('../telegram')
 const irc = require('../irc')
 
+const onIRCMessage = (to, message) => {
+  telegram.sendMessage(to, message)
+}
+
+const onTelegramMessage = (from, msg, chat) => {
+  irc.sendMessage(from, msg, chat)
+}
+
 const init = async () => {
   await irc.init({
-    onMessage: telegram.sendMessage
+    onMessage: onIRCMessage
   })
   await telegram.init({
-    onMessage: irc.sendMessage
+    onMessage: onTelegramMessage
   })
 }
 
